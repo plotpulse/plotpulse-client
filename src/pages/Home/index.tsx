6af6f951fff3982f6130837f363bc230a8303f75
mgballou <matthewbuiltthat@gmail.com>
2023-10-03 21:34:17 -0400

    task: refactor into pagewrapper component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,4 @@
-import {
-    Box,
-} from "@chakra-ui/react";
-import { motion } from "framer-motion";
-
-import { Hero } from '../../components'
+import { Hero, PageWrapper } from '../../components'
 
 
 
@@ -11,18 +6,12 @@ import { Hero } from '../../components'
 export function Home() {
 
     return (
-        <Box
-            as={motion.div}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transitionDuration={'1'}
-            exit={{ opacity: 0, transition: { duration: 0.5 } }}
-        >
+        <PageWrapper>
             <Hero />
 
 
 
 
-        </Box>
+        </PageWrapper>
     );
 }
--- src/pages/PromptShow/index.tsx
+++ src/pages/PromptShow/index.tsx
@@ -1,26 +1,19 @@
-import { Box } from "@chakra-ui/react";
-import { motion } from "framer-motion";
+import { PageWrapper } from "../../components";
 
 export function PromptShow(){
 
     
     return (
-        <Box
-            as={motion.div}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transitionDuration={'1'}
-            exit={{ opacity: 0, transition: { duration: 0.5 } }}
-        >
+        <PageWrapper>
             <p>
-                Prompts go here
+                Prompt goes here
                 
                 </p>
                 
 
 
 
-        </Box>
+        </PageWrapper>
     )
 }
 
