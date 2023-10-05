262baf5bba3c1fb2fe0db83aae0cc8335c22efbf
mgballou <matthewbuiltthat@gmail.com>
2023-10-05 14:49:18 -0400

    task: howitWorks heading, spacing , margins

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,49 +1,56 @@
-import { Card, CardHeader, Heading, CardBody, CardFooter, Button, Text, SimpleGrid } from "@chakra-ui/react";
+import { Card, CardHeader, Heading, CardBody, CardFooter, Button, Text, SimpleGrid, Box } from "@chakra-ui/react";
 
 
-export function HowItWorks(){
+export function HowItWorks() {
 
     return (
+        <Box w={'full'} maxW={'xxl'}>
+        
+        <Heading size='lg' textAlign={'center'} mb={8}>How It Works</Heading>
 
-        <SimpleGrid spacing={4} columns={{sm: 1, md: 3}} mx={4} >
-        <Card>
-            <CardHeader>
-                <Heading size='md'> Customer dashboard</Heading>
-            </CardHeader>
-            <CardBody>
-                <Text>View a summary of all your customers over the last month.</Text>
-            </CardBody>
-            <CardFooter>
-                <Button>View here</Button>
-            </CardFooter>
-        </Card>
-
-
-
-        <Card>
-            <CardHeader>
-                <Heading size='md'> Customer dashboard</Heading>
-            </CardHeader>
-            <CardBody>
-                <Text>View a summary of all your customers over the last month.</Text>
-            </CardBody>
-            <CardFooter>
-                <Button>View here</Button>
-            </CardFooter>
-        </Card>
+       
+        <SimpleGrid spacing={4} columns={{ sm: 1, md: 3 }} mx={12} >
+            <Card>
+                <CardHeader>
+                    <Heading size='md'> Customer dashboard</Heading>
+                </CardHeader>
+                <CardBody>
+                    <Text>View a summary of all your customers over the last month.</Text>
+                </CardBody>
+                <CardFooter>
+                    <Button>View here</Button>
+                </CardFooter>
+            </Card>
 
 
-        <Card>
-            <CardHeader>
-                <Heading size='md'> Customer dashboard</Heading>
-            </CardHeader>
-            <CardBody>
-                <Text>View a summary of all your customers over the last month.</Text>
-            </CardBody>
-            <CardFooter>
-                <Button>View here</Button>
-            </CardFooter>
-        </Card>
-    </SimpleGrid>
+
+            <Card>
+                <CardHeader>
+                    <Heading size='md'> Customer dashboard</Heading>
+                </CardHeader>
+                <CardBody>
+                    <Text>View a summary of all your customers over the last month.</Text>
+                </CardBody>
+                <CardFooter>
+                    <Button>View here</Button>
+                </CardFooter>
+            </Card>
+
+
+            <Card>
+                <CardHeader>
+                    <Heading size='md'> Customer dashboard</Heading>
+                </CardHeader>
+                <CardBody>
+                    <Text>View a summary of all your customers over the last month.</Text>
+                </CardBody>
+                <CardFooter>
+                    <Button>View here</Button>
+                </CardFooter>
+            </Card>
+        </SimpleGrid>
+        </Box>
+
+
     )
 }
