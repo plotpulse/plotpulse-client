5aaccb28ee1ab3ce741b564e112596d4958a869e
mgballou <matthewbuiltthat@gmail.com>
2023-10-05 21:37:13 -0400

    task: draft copy in howitworks

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,7 +1,9 @@
-import { Card, CardHeader, Heading, CardBody, CardFooter, Button, Text, SimpleGrid, Box } from "@chakra-ui/react";
+import { Card, CardHeader, Heading, CardBody, CardFooter, Button, Text, SimpleGrid, Box, useColorModeValue, Divider } from "@chakra-ui/react";
 
 
 export function HowItWorks() {
+
+    const accentVal = useColorModeValue('accent.700', 'accent.200')
 
     return (
         <Box w={'full'} maxW={'xxl'}>
@@ -12,13 +14,14 @@ export function HowItWorks() {
         <SimpleGrid spacing={4} columns={{ sm: 1, md: 3 }} mx={12} >
             <Card>
                 <CardHeader>
-                    <Heading size='md'> Customer dashboard</Heading>
+                    <Heading size='md'><Text as='span' color={accentVal}>1. </Text>Sign Up for a Free Account</Heading>
                 </CardHeader>
+                <Divider/>
                 <CardBody>
-                    <Text>View a summary of all your customers over the last month.</Text>
+                    <Text>Create an account and a user profile. What type of content are you interested in? What genres are your favorite? What type of worlds do you want to build?</Text>
                 </CardBody>
                 <CardFooter>
-                    <Button>View here</Button>
+                    <Button>Fix this button</Button>
                 </CardFooter>
             </Card>
 
@@ -26,26 +29,28 @@ export function HowItWorks() {
 
             <Card>
                 <CardHeader>
-                    <Heading size='md'> Customer dashboard</Heading>
+                    <Heading size='md'><Text as='span' color={accentVal}>2. </Text> Get Inspired</Heading>
                 </CardHeader>
+                <Divider/>
                 <CardBody>
-                    <Text>View a summary of all your customers over the last month.</Text>
+                    <Text>View all of our user-submitted prompts. Find the inspiration for your next campaign, character build, or the next beat in your sprawling novel. Post replies to prompts and submit your own to keep the creative juices flowing!</Text>
                 </CardBody>
                 <CardFooter>
-                    <Button>View here</Button>
+                    <Button>All Prompts</Button>
                 </CardFooter>
             </Card>
 
 
             <Card>
                 <CardHeader>
-                    <Heading size='md'> Customer dashboard</Heading>
+                    <Heading size='md'><Text as='span' color={accentVal}>3. </Text>Check Back Soon</Heading>
                 </CardHeader>
+                <Divider/>
                 <CardBody>
-                    <Text>View a summary of all your customers over the last month.</Text>
+                    <Text>Plot Pulse is growing, and we've got a ton of new features coming down the pipeline. Soon, you'll be able to use our platform to fully plan and document everything related to your unique creations.</Text>
                 </CardBody>
                 <CardFooter>
-                    <Button>View here</Button>
+                    <Button>Coming soon!</Button>
                 </CardFooter>
             </Card>
         </SimpleGrid>
