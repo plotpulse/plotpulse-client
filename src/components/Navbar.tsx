981b90a25766b5c7db68dd9f275449b305dfd138
mgballou <matthewbuiltthat@gmail.com>
2023-10-10 22:44:13 -0400

    task: wip - connecting profile to backend and auth0

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import { HStack, Link as ChakraLink, Button, useColorMode, useColorModeValue, IconButton, Box, Flex, Heading, Image, MenuButton, Menu, MenuList, MenuItem } from "@chakra-ui/react"
 import { Link as RouterLink } from 'react-router-dom'
-import { LoginButton, LogoutButton, SignUpButton } from "."
 import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 import { useAuth0 } from '@auth0/auth0-react'
 
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { PageWrapper } from "../../components"
+import { useState, useEffect } from "react";
+import { PageWrapper, SignUpForm } from "../../components"
 import { useAuth0 } from "@auth0/auth0-react";
 import { IProfile } from "../../shared-types";
 import { getProfile, createProfile } from "../../utilities/auth-services";
@@ -8,30 +8,45 @@ import { Button, Text } from "@chakra-ui/react";
 
 
 
-export function Profile(){
+export function Profile() {
 
     const { user, getAccessTokenSilently, loginWithRedirect } = useAuth0()
-    const [ profile, setProfile ] = useState<IProfile | null>(null)
+    const [profile, setProfile] = useState<IProfile | null>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(true)
 
     // if (!user){
     //     loginWithRedirect()
     // }
-   
-            
-            
-            
 
-    async function handleRequest(){
+    const email = user?.email ?? ""
+
+    const mockProfile: IProfile = {
+        id: email,
+        genres: "a, b, c",
+        bio: "abc"
+    }
+
+    async function handleFetchProfile() {
+
+        try {
+            const response = await getProfile(await getAccessTokenSilently(), email)
+            setProfile(response)
+            setIsLoading(false)
+            
+        } catch (error) {
+            
+        }
+
+    }
+
+
+
+
+    async function handleNewProfile() {
         try {
             if (!user) return
-            const email = user?.email ?? ""
 
-            const mockProfile: IProfile = {
-                id: email,
-                genres: "a, b, c",
-                bio: "abc"
-            }
-        
+
             const profileResponse = await createProfile(mockProfile)
             console.log(profileResponse)
 
@@ -39,25 +54,20 @@ export function Profile(){
             // const token = await getAccessTokenSilently()
             // const profResponse = await getProfile(token, email)
 
-
-
-            // if there is a profile, we will display the profile
-
-
-
-
-            // if there is no profile, we will display the create profile UI
-            
         } catch (error) {
-            
+
         }
     }
+
+    useEffect(() => { handleFetchProfile() }, [isLoading])
 
     return (
         <PageWrapper>
             <Text>{user ? "Hello" : "Please log in"}</Text>
 
-            <Button onClick={handleRequest}>Request handler</Button>
+            <Button onClick={handleNewProfile}>Request handler</Button>
+
+            {/* {profile ? <Dashboard /> : <SignUpForm />} */}
 
 
 
