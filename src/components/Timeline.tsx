b945a7b7c2621fc27e33fe617260e24ac58cf19e
mgballou <matthewbuiltthat@gmail.com>
2023-10-14 17:14:29 -0400

    task: wip - cards for displaying prompts in timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,22 +1,59 @@
 import { IPrompt } from "../shared-types";
 
+import { Text, Card, CardHeader, CardBody, CardFooter, HStack, Button, Box, VStack } from "@chakra-ui/react";
+
+import { AddIcon, StarIcon } from '@chakra-ui/icons'
+
 interface TimeLineProps {
     prompts: IPrompt[] | null;
 }
 
+interface PromptCardProps {
+    prompt: IPrompt;
+}
 
-export function Timeline({prompts}: TimeLineProps){
+function PromptCard({ prompt }: PromptCardProps) {
+    const { id, content, user, likes, replies, } = prompt
+
+
+    return (
+        <Card>
+            <CardHeader>#{id} from @{user.id}</CardHeader>
+            <CardBody>
+                <Text>{content}</Text>
+            </CardBody>
+            <CardFooter>
+                <HStack>
+                    <HStack>
+                        <Text>{replies?.length}</Text>
+                        <Button leftIcon={<AddIcon/>}>Reply</Button>
+                    </HStack>
+
+                    <HStack>
+                        <Text>{likes?.length}</Text>
+                        <Button leftIcon={<StarIcon/>}>Like</Button>
+                    </HStack>
+                </HStack>
+            </CardFooter>
+        </Card>
+
+    )
+}
+
+
+
+export function Timeline({ prompts }: TimeLineProps) {
 
     const cards = prompts?.map(prompt => {
         return (
-        <p key={prompt.id}>{prompt.content}</p>
+            <PromptCard key={prompt.id} prompt={prompt} />
         )
     })
 
     return (
         <>
-        {cards}
-        
+            {cards}
+
         </>
     )
 }
