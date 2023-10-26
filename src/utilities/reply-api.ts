6a17974f30866ff627886aa19753744c23b8e848
mgballou <matthewbuiltthat@gmail.com>
2023-10-25 23:39:15 -0400

    task: createReply()

--- src/utilities/reply-api.ts
+++ src/utilities/reply-api.ts
@@ -1,4 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import { IReply } from '../shared-types'
 const { getAccessTokenSilently } = useAuth0()
 const PROMPT_URL = import.meta.env.VITE_PROMPT_URL
 
@@ -25,6 +26,37 @@ export async function getAll(promptId: number){
         }
         
     } catch (error) {
+        return error
+        
+    }
+
+}
+
+export async function create(promptId:number, newReply: IReply){
+
+    try {
+
+        const options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `bearer ${ await getAccessTokenSilently()}`
+            },
+            body: JSON.stringify(newReply)
+        }
+
+        const replyURL = `${PROMPT_URL}/${promptId}`
+
+        const response = await fetch(replyURL, options)
+
+        if (response.ok){
+            return response.json()
+        } else {
+            throw new Error('Invalid request')
+        }
+        
+    } catch (error) {
+        console.log(error)
         return error
         
     }
--- src/utilities/reply-service.ts
+++ src/utilities/reply-service.ts
@@ -1,14 +1,29 @@
+import { IReply } from '../shared-types'
 import * as replyAPI from './reply-api'
 
-export async function getReplies(promptId: number){
+export async function getReplies(promptId: number) {
     try {
         const allReplies = await replyAPI.getAll(promptId)
         return allReplies
 
-        
+
     } catch (error) {
         console.log(error)
         throw new Error('Could not fetch replies.')
-        
+
+    }
+}
+
+export async function createReply(promptId: number, newReply: IReply) {
+
+    try {
+        const replyResponse = await replyAPI.create(promptId, newReply)
+        return replyResponse
+
+    } catch (error) {
+        console.log(error)
+        throw new Error('Could not create reply')
+
+
     }
 }
