6b5a774ead1e66dd2ebf1ca7f3abe1e416ec8fb0
mgballou <matthewbuiltthat@gmail.com>
2023-11-02 01:06:34 -0400

    task: jwt working on localhost

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN
 const auth0ClientId = import.meta.env.VITE_AUTH0_CLIENT_ID
+const auth0Audience = import.meta.env.VITE_AUTH0_AUDIENCE
 if (!auth0Domain || !auth0ClientId) {
     throw new Error("Auth0 environment variables are not set.")
   }
@@ -17,6 +18,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <Auth0Provider
     domain={auth0Domain}
     clientId={auth0ClientId}
+    authorizationParams={{
+      audience: auth0Audience
+    }}
     >
       <ChakraProvider theme={theme}>
         <Router>
